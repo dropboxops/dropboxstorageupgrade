c6e21c263fb3f3817f5beadddec7a1c7d3bd7f95
dropboxops <shophicropter12@outlook.com>
2026-07-24 17:43:18 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+extensions = [ Dropbox Storage Upgrade]
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
